82937a5d2c43eacd894a7c1e34cd1c5d5712344e
SwenenzY <44095096+SwenenzY@users.noreply.github.com>
2019-01-15 19:32:04 +0300

    x
    x

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ client.on('ready', () =>{
 })
 
 client.on('message', message =>{
-  //check channel from the message recieved
-let tStamp = moment().format('LLLL'); //creates time stamp
 
-if (message.channel.type == 'dm'){ //checks for DM - Creates a DM DB and records
+let tStamp = moment().format('LLLL');
+
+if (message.channel.type == 'dm'){ 
   let dmName = `${message.author.username}DM`;
   sql.run(`INSERT INTO ${dmName} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]).catch(()=>{
     sql.run(`CREATE TABLE IF NOT EXISTS ${dmName} (username TEXT, message TEXT, timestamp TEXT, userID TEXT);`).then(() =>{
       sql.run(`INSERT INTO ${dmName} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]);
     })
   })
-}else{ //otherwise it's a normal channel
+}else{ 
   sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]).catch(() =>{
     console.error;
     sql.run(`CREATE TABLE IF NOT EXISTS ${message.channel.name} (username TEXT, message TEXT, timestamp TEXT, userID TEXT);`).then(() =>{
       sql.run(`INSERT INTO ${message.channel.name} (username, message, timestamp, userID) VALUES (?,?,?,?)`, [message.author.username, message.content, tStamp, message.author.id]);
-    }) // KNOWN ISSUE: Does not like channels with "-" in them
+    }) 
   })
 }
 });
